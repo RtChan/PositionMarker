b9f14c86a6306153b0d155618b724bfaacbebe44
FlyTira <303380876@qq.com>
2016-08-26 16:34:44 +0800

    实现在ZXingQRFragment中显示并翻页二维码
    每页二维码最多含有20组数据

--- app/src/main/java/com/gzcz/rtchen/positionmarker/ZXingQRFragment.java
+++ app/src/main/java/com/gzcz/rtchen/positionmarker/ZXingQRFragment.java
@@ -1,12 +1,19 @@
 package com.gzcz.rtchen.positionmarker;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.zxing.WriterException;
 
 
 /**
@@ -17,7 +24,7 @@ import android.view.ViewGroup;
  * Use the {@link ZXingQRFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ZXingQRFragment extends Fragment {
+public class ZXingQRFragment extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,26 +36,112 @@ public class ZXingQRFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    int page_number = 1;
+    int now_page = 1;
+    int enter_number = 0;
+    int now_enter_number = 0;
+    String QRcodebuf = "";
+    String Display_part = "";
+    TextView textView = null;
+    ImageView qrImageView = null;
+    Button btn_previous_page = null;
+    Button btn_next_page = null;
+
     public ZXingQRFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ZXingQRFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static ZXingQRFragment newInstance(String param1, String param2) {
+    public static ZXingQRFragment newInstance(String QRcodebuf) {
         ZXingQRFragment fragment = new ZXingQRFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("QRcodebuf",QRcodebuf);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public void Judge_number(String QRcodebuf){
+        for(int i = 0; i < QRcodebuf.length(); i++){
+            if (QRcodebuf.charAt(i) == '\n'){
+                page_number = page_number + 1;
+            }
+        }
+
+        page_number = (page_number - 1) / 20 + 1;
+    }
+
+    public void clone_QRcodebuf(){
+        Display_part = "";
+        int i = 0;
+        enter_number = 0;
+        now_enter_number = 20 * (now_page - 1);
+
+        if(now_enter_number != 0){
+            for(; i < QRcodebuf.length(); i++){
+                if (QRcodebuf.charAt(i) == '\n'){
+                    enter_number = enter_number + 1;
+                }
+
+                if (enter_number == now_enter_number){
+                    break;
+                }
+            }
+
+            i = i + 1;
+        }
+
+        for(; (i < QRcodebuf.length()) && (now_enter_number < (20 * now_page)); i++){
+            if (QRcodebuf.charAt(i) == '\n'){
+                now_enter_number = now_enter_number + 1;
+            }
+
+            Display_part = Display_part + QRcodebuf.charAt(i);
+        }
+    }
+
+    @Override
+    public void onClick(View view){
+
+        switch (view.getId()){
+            case R.id.btn_previous_page:{
+                Log.d("TAG", "onClick: "+Integer.toString(now_page)+Integer.toString(page_number));
+                if (now_page > 1){
+                    now_page = now_page - 1;
+                    clone_QRcodebuf();
+
+                    textView.setText(String.valueOf(now_page) + "/" + String.valueOf(page_number));
+
+                    ZXingQR zxingQR = new ZXingQR();
+                    try {
+                        Bitmap qrCodeBitmap = zxingQR.createQRCode(Display_part, 1000);
+                        qrImageView.setImageBitmap(qrCodeBitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                break;
+            }
+
+            case R.id.btn_next_page:{
+                Log.d("TAG", "onClick: "+Integer.toString(now_page)+Integer.toString(page_number));
+                if (now_page < page_number){
+                    now_page = now_page + 1;
+                    clone_QRcodebuf();
+
+                    textView.setText(String.valueOf(now_page) + "/" + String.valueOf(page_number));
+
+                    ZXingQR zxingQR = new ZXingQR();
+                    try {
+                        Bitmap qrCodeBitmap = zxingQR.createQRCode(Display_part, 1000);
+                        qrImageView.setImageBitmap(qrCodeBitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                break;
+            }
+        }
     }
 
     @Override
@@ -63,8 +156,32 @@ public class ZXingQRFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.zxingqr_fragment, container,false);
+
+        textView = (TextView)mView.findViewById(R.id.text_page_number);
+        qrImageView = (ImageView)mView.findViewById(R.id.iv_qr_image);
+        btn_previous_page = (Button)mView.findViewById(R.id.btn_previous_page);
+        btn_next_page = (Button)mView.findViewById(R.id.btn_next_page);
+        btn_previous_page.setOnClickListener(this);
+        btn_next_page.setOnClickListener(this);
+
+        if(getArguments() != null){
+            QRcodebuf = getArguments().getString("QRcodebuf");
+            Judge_number(QRcodebuf);
+        }
+
+        textView.setText(String.valueOf(now_page) + "/" + String.valueOf(page_number));
+
+        ZXingQR zxingQR = new ZXingQR();
+        try {
+            Bitmap qrCodeBitmap = zxingQR.createQRCode(QRcodebuf, 1000);
+            qrImageView.setImageBitmap(qrCodeBitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return mView;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.zxingqr_fragment, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
